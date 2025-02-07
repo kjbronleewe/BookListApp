8c927d98a85163753bc15618f6e74623c23e9d90
Kelly Bronleewe <kjbronleewe@gmail.com>
2025-02-06 18:46:10 -0800

    Local Storage

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import Header from './components/Header';
 import './App.css';
 import BookList from './components/BookList';
 import AddBook from './components/AddBook';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
+  const [books, setBooks] = useLocalStorage('books', [])
+
   return (
     <div className="App">
       <Header />
--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,51 +1,60 @@
 import { Button, Col, FormGroup, Label } from 'reactstrap';
 import { Formik, Form, Field } from 'formik';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 const BookForm = () => {
+  const [books, setBooks] = useLocalStorage('books', []);
 
   const handleSubmit = (values, { resetForm }) => {
-    console.log('form values:', values);
-    console.log('in JSON format:', JSON.stringify(values));
+    const newBook = {
+      id: Date.now(),
+      title: values.title,
+      author: values.author,
+      rating: values.rating,
+      completed: values.completed,
+    };
+    setBooks((prevBooks) => [...prevBooks, newBook]);
+
     resetForm();
   };
 
   return (
     <Formik
-    initialValues={{
-      title: '', 
-      author: '', 
-      rating: '',
-      completed: false
-    }}
-    onSubmit={handleSubmit}
+      initialValues={{
+        title: '',
+        author: '',
+        rating: '',
+        completed: false,
+      }}
+      onSubmit={handleSubmit}
     >
       <Form>
         <FormGroup row>
-          <Label htmlFor='title' md='2'>
+          <Label htmlFor="title" md="2">
             Title
           </Label>
-          <Col md='5'>
-            <Field 
-            name='title'
-            placeholder='Title of Book'
-            className='form-control'
+          <Col md="5">
+            <Field
+              name="title"
+              placeholder="Title of Book"
+              className="form-control"
             />
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label htmlFor='author' md='2'>
+          <Label htmlFor="author" md="2">
             Author
           </Label>
-          <Col md='5'>
-            <Field 
-            name='author'
-            placeholder='Book Author'
-            className='form-control'
+          <Col md="5">
+            <Field
+              name="author"
+              placeholder="Book Author"
+              className="form-control"
             />
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label htmlFor='rating' md='2'>
+          <Label htmlFor="rating" md="2">
             Book rating
           </Label>
           <Col md="3">
@@ -61,10 +70,13 @@ const BookForm = () => {
         </FormGroup>
         <FormGroup row>
           <Label check md={{ size: 10, offset: 1 }}>
-            <Field name="completed" type="checkbox" className="form-check-input" />{' '}
+            <Field
+              name="completed"
+              type="checkbox"
+              className="form-check-input"
+            />{' '}
             Have you completed this book?
           </Label>
-
         </FormGroup>
         <FormGroup row>
           <Col md={{ size: 10, offset: 1 }}>
@@ -75,8 +87,15 @@ const BookForm = () => {
         </FormGroup>
       </Form>
     </Formik>
-
   );
 };
+
+const BookList = ({ books }) => (
+  <ul>
+    {books.map((book) => (
+      <li key={book.id}>{book.title} by {book.author}</li>
+    ))}
+  </ul>
+);
 
 export default BookForm;
